3f3725d38413503d1860765232f2a2f7ed35bff3
Thierry Goettelmann <thierry@byscripts.info>
2014-04-27 12:10:17 +0200

    Add gi for .gitignore generation

---
+++ functions/gi.fish
@@ -0,0 +1,9 @@
+function gi --description 'Generate a .gitignore file from gitignore.io'
+	if count $argv > /dev/null
+		curl http://www.gitignore.io/api/$argv
+	else
+		echo "Usage: gi choice1,choice2,choiceN >> .gitignore"
+		echo ""
+		echo "To get a list of possible choices: gi list"
+	end
+end
