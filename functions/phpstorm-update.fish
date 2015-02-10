1d41c52a832caa597ed13316c24bd88e907f750d
Thierry Goettelmann <thierry@byscripts.info>
2015-02-10 13:04:07 +0100

    add phpstorm-update function

---
+++ functions/phpstorm-update.fish
@@ -0,0 +1,28 @@
+function phpstorm-update -a url
+
+    if test -z $url
+        __red "Usage: phpstorm-update <url>"
+        return
+    end
+
+
+    if test -d "$HOME/Applications/PhpStorm"
+        set -l TODAY (date +"%Y-%m-%d-%H-%M")
+        mv "$HOME/Applications/PhpStorm" "$HOME/Applications/PhpStorm-$TODAY"
+    end
+
+    mkdir -p "$HOME/Applications/PhpStorm"
+
+    echo -n "Downloading $url... "
+    set -l STORM_FILENAME (basename "$url")
+
+    echo -n "    "
+    wget -P "/tmp" --progress=dot $url 2>&1 | grep --line-buffered "%" | sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
+    echo -ne "\b\b\b\b"
+    __green "OK"
+
+    echo -n "Extracting $STORM_FILENAME... "
+    tar --strip-components=1 -C "$HOME/Applications/PhpStorm" -xzf "/tmp/$STORM_FILENAME"
+    __green "OK"
+
+end
