1dd96ff506ec3af01979fed88f6e2b64dbac1879
Thierry Goettelmann <thierry@byscripts.info>
2015-08-06 23:14:11 +0200

    Update abbreviations

--- abbreviations.fish
+++ abbreviations.fish
@@ -1,17 +1,9 @@
-# Artisan
-abbr a php artisan
-
 # Apache
 abbr a2reload sudo service apache2 reload
 abbr a2restart sudo service apache2 restart
 
-# Composer
-abbr c composer
-abbr ci composer install
-abbr cr composer require
-abbr csu sudo composer self-update
-abbr cu composer update
-abbr cv composer validate
+# Artisan
+abbr a php artisan
 
 # Apt-Get
 abbr ar sudo apt-get autoremove
@@ -21,11 +13,13 @@ abbr r sudo apt-get remove
 abbr u sudo apt-get update
 abbr ug sudo apt-get upgrade
 
-# Symfony
-abbr s php app/console
-
-# LS
-abbr l ls
+# Composer
+abbr c composer
+abbr ci composer install
+abbr cr composer require
+abbr csu sudo composer self-update
+abbr cu composer update
+abbr cv composer validate
 
 # Git
 abbr g git
@@ -41,8 +35,29 @@ abbr gs git status
 abbr gc git clone
 abbr gd git diff
 
-# PS
-abbr psg ps -ef | grep
+# LS
+abbr l ls
+
+# NPM
+abbr ni npm install --save-dev
+abbr np npm prune
+abbr nl npm list
+abbr nr npm remove
+abbr nu npm update
+
+# PGREP
+abbr pg pgrep -l
+
+# Symfony
+abbr s php app/console
+abbr scc php app/console cache:clear
+abbr sgb php app/console generate:bundle
+abbr sgc php app/console generate:controller
+abbr sdge php app/console doctrine:generate:entity
+abbr sdsc php app/console doctrine:schema:create
+abbr sdsu php app/console doctrine:schema:update
+abbr sdsv php app/console doctrine:schema:validate
+abbr sddc php app/console doctrine:database:create
 
 # Other
 abbr add_to_path set -U fish_user_paths $fish_user_paths
