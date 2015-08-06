95a598b2a3d79932318c078a9a0182fd81fbeb06
Thierry Goettelmann <thierry@byscripts.info>
2015-08-06 23:32:22 +0200

    Update abbreviations

--- abbreviations.fish
+++ abbreviations.fish
@@ -1,64 +1,64 @@
 # Apache
-abbr a2reload sudo service apache2 reload
-abbr a2restart sudo service apache2 restart
+abbr a2reload 'sudo service apache2 reload'
+abbr a2restart 'sudo service apache2 restart'
 
 # Artisan
-abbr a php artisan
+abbr a 'php artisan'
 
 # Apt-Get
-abbr ar sudo apt-get autoremove
-abbr dug sudo apt-get dist-upgrade
-abbr i sudo apt-get install
-abbr r sudo apt-get remove
-abbr u sudo apt-get update
-abbr ug sudo apt-get upgrade
+abbr ar 'sudo apt-get autoremove'
+abbr dug 'sudo apt-get dist-upgrade'
+abbr i 'sudo apt-get install'
+abbr r 'sudo apt-get remove'
+abbr u 'sudo apt-get update'
+abbr ug 'sudo apt-get upgrade'
 
 # Composer
-abbr c composer
-abbr ci composer install
-abbr cr composer require
-abbr csu sudo composer self-update
-abbr cu composer update
-abbr cv composer validate
+abbr c 'composer'
+abbr ci 'composer install'
+abbr cr 'composer require'
+abbr csu 'sudo composer self-update'
+abbr cu 'composer update'
+abbr cv 'composer validate'
 
 # Git
-abbr g git
-abbr ga. git add .
-abbr ga git add
-abbr gcm git commit -m
-abbr gco git checkout
-abbr gi gitignore
-abbr gm git merge
-abbr gpl git pull
-abbr gps git push
-abbr gs git status
-abbr gc git clone
-abbr gd git diff
+abbr g 'git'
+abbr ga. 'git add .'
+abbr ga 'git add'
+abbr gcm 'git commit -m'
+abbr gco 'git checkout'
+abbr gi 'gitignore'
+abbr gm 'git merge'
+abbr gpl 'git pull'
+abbr gps 'git push'
+abbr gs 'git status'
+abbr gc 'git clone'
+abbr gd 'git diff'
 
 # LS
-abbr l ls
+abbr l 'ls'
 
 # NPM
-abbr ni npm install --save-dev
-abbr np npm prune
-abbr nl npm list
-abbr nr npm remove
-abbr nu npm update
+abbr ni 'npm install --save-dev'
+abbr np 'npm prune'
+abbr nl 'npm list'
+abbr nr 'npm remove'
+abbr nu 'npm update'
 
 # PGREP
-abbr pg pgrep -l
+abbr pg 'pgrep -l'
 
 # Symfony
-abbr s php app/console
-abbr scc php app/console cache:clear
-abbr sgb php app/console generate:bundle
-abbr sgc php app/console generate:controller
-abbr sdge php app/console doctrine:generate:entity
-abbr sdsc php app/console doctrine:schema:create
-abbr sdsu php app/console doctrine:schema:update
-abbr sdsv php app/console doctrine:schema:validate
-abbr sddc php app/console doctrine:database:create
+abbr s php 'app/console'
+abbr scc php 'app/console cache:clear'
+abbr sgb php 'app/console generate:bundle'
+abbr sgc php 'app/console generate:controller'
+abbr sdge php 'app/console doctrine:generate:entity'
+abbr sdsc php 'app/console doctrine:schema:create'
+abbr sdsu php 'app/console doctrine:schema:update'
+abbr sdsv php 'app/console doctrine:schema:validate'
+abbr sddc php 'app/console doctrine:database:create'
 
 # Other
-abbr add_to_path set -U fish_user_paths $fish_user_paths
-abbr freebox_mount sudo mount -t cifs //mafreebox.freebox.fr/Disque\ dur /media/freebox -o user=freebox,uid=1000,gid=1000,rw,password=
+abbr add_to_path 'set -U fish_user_paths $fish_user_paths'
+abbr freebox_mount 'sudo mount -t cifs //mafreebox.freebox.fr/Disque\\ dur /media/freebox -o user=freebox,uid=1000,gid=1000,rw,password='
